4f94a6bb7f12b5f4ea171466b86157020a4e197c
paribhasika <paribhasika.006@gmail.com>
2023-04-15 03:01:54 +0100

    chore: initial setup

---
+++ apps/products/next.config.js
@@ -0,0 +1,37 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: false,
+  },
+  compiler:{
+    styledComponents: true
+  },
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'products',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: {},
+        exposes: {
+          './pdp': './src/pages/pdp/[...slug]'
+            },
+            shared:{
+              '@emotion/': {
+                eager: true,
+                requiredVersion: false,
+                singleton: true,
+              },
+              '@chakra-ui/': {
+                eager: true,
+                requiredVersion: false,
+                singleton: true,
+              },
+            }
+      })
+    );
+
+    return config;
+  },
+};
